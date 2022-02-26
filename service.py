e9517f50cd64fd71da51fab3728c3317a708de6a
Bert Davies <bertdaviezz@gmail.com>
2022-02-26 11:48:14 -0800

    add socket connection, fix logging and date/hour bug

--- service.py
+++ service.py
@@ -8,13 +8,18 @@ import os
 import glob
 from pydub import AudioSegment
 from multiprocessing import Process
+import socket
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
 CHUNK = 1024 #CHUNK is frames in buffer
 FORMAT = pyaudio.paInt24 #must save in format 24
 CHANNELS = 2 #CHANNELS is stereo
 RATE = 32000 #new setting at 32
+
+PORT = 5001
 
 p = pyaudio.PyAudio()
 
@@ -24,17 +29,16 @@ stream = p.open(format=FORMAT,
             input=True,
             frames_per_buffer=CHUNK)
 
-def setup_logger(name, log_file, level=logging.INFO):
+def setup_logger(logger, log_file, level=logging.INFO):
     """To setup as many loggers as you want"""
 
-    handler = logging.FileHandler(log_file)        
+    handler = logging.FileHandler(filename=log_file)
+    handler.setLevel(level)
     handler.setFormatter(formatter)
 
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
     logger.addHandler(handler)
 
-    return logger
+    return handler
 
 # Convert all of the .wav files in a given directory to .flac files
 
@@ -59,38 +63,51 @@ def wav_to_flac(dir):
 # file are created and stored in the innermost file. 
 
 def start_logging():
+    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         while True:
+            try:
+                socket_obj.bind(('', PORT))
+                socket_obj.listen(1) # Only allow one connection for now
+                conn, addr = socket_obj.accept()
+                conn.setblocking(False) # Don't block waiting for data from this connection
+                print('Connected by: ', addr)
+            except socket.error:
+                pass
             dt_outer = datetime.now()
 
             while True:
                 dt_mid = datetime.now()
-                path = dt_mid.strftime('/home/bert/audio_log_data/%Y_%m_%d')
+                if dt_mid.date() != dt_outer.date(): # how often to make a main dir change to 1 day
+                    break
+                path = dt_mid.strftime('/media/pi/DDI_UCSC_LOGGING/audio_log_data/%Y_%m_%d')
                 if not os.path.exists(path):
                     os.makedirs(path)
 
-                if dt_mid - dt_outer > timedelta(days=1): # how often to make a main dir change to 1 day
-                    break
                 path_inner = ''
+                handler = None
                 while True:
                     dt_inner = datetime.now()
-                    if dt_inner - dt_mid > timedelta(minutes=60): # how often to make a subdir change to 1 hour
+                    if dt_inner.hour != dt_mid.hour: # how often to make a subdir change to 1 hour
+                        logger.removeHandler(handler)
+                        del handler
+                        gc.collect()
                         break
 
                     old_path = path_inner
                     path_inner = path + '/' + dt_inner.strftime('%H')
                     if not os.path.exists(path_inner):
                         os.makedirs(path_inner)
+                        log_string = path_inner + '/' + dt_inner.strftime('logfile_%H.log')
+                        handler = setup_logger(logger, log_string)
+                        logger.info('Log start')
                         proc = Process(target=wav_to_flac, args=(old_path,), daemon=True)
                         proc.start()
-                    
+
                     frames = []
-                    log_string = path_inner + '/' + dt_inner.strftime('logfile_%H:%M:%S.log')
                     wav_string = path_inner + '/' + dt_inner.strftime('soundfile_%H:%M:%S.wav')
 
                     wf = wave.open(wav_string, 'wb')
-                    logger = setup_logger('audio_logger', log_string)
-                    logger.info('Example of logging with ISO-8601 timestamp')
                     end_time = dt_inner + timedelta(minutes=5) # how long our sound files are, change to 5 minutes
 
                     while True:
@@ -99,9 +116,13 @@ def start_logging():
                             break
                         data = stream.read(CHUNK, exception_on_overflow=False)
                         frames.append(data)
+                        try:
+                            data = conn.recv(1024)
+                            if data:
+                                logger.info(data.decode())
+                        except Exception:
+                            pass
                         # conn.send(data)
-                    del logger
-                    gc.collect()
 
                     wf.setnchannels(CHANNELS)
                     wf.setsampwidth(p.get_sample_size(FORMAT))
